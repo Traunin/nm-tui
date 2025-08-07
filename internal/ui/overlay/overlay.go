31e46ed1b7d37a588c02b937271af0aafd88ec35
alphameo <pavel.2014.play@gmail.com>
2025-08-08 00:30:47 +0300

    refactor(overlay): change escape mapping

--- internal/ui/overlay/overlay.go
+++ internal/ui/overlay/overlay.go
@@ -38,7 +38,7 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "q", "esc", "ctrl+c":
+		case "ctrl+q", "esc", "ctrl+c":
 			m.IsActive = false
 			return m, nil
 		}
