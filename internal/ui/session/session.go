38fb3214625bf00923f5603bd18169e5f807cd32
alphameo <pavel.2014.play@gmail.com>
2025-08-08 00:31:26 +0300

    feat(session): add escape mapping

--- internal/ui/session/session.go
+++ internal/ui/session/session.go
@@ -73,7 +73,7 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		switch msg := msg.(type) {
 		case tea.KeyMsg:
 			switch msg.String() {
-			case "q", "esc", "ctrl+c":
+			case "q", "ctrl+q", "esc", "ctrl+c":
 				return m, tea.Quit
 			case "tab":
 				if m.state == wifiView {
