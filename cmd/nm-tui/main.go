3252dc2f53e59f84e59f19d515d1489a29e04c63
alphameo <pavel.2014.play@gmail.com>
2025-08-09 12:17:11 +0300

    feat: logger on open/close

--- cmd/nm-tui/main.go
+++ cmd/nm-tui/main.go
@@ -10,9 +10,10 @@ import (
 
 func main() {
 	logger.Init("./log")
+	logger.InfoLog.Println("The program is running")
+	defer logger.InfoLog.Println("Program is closed")
 	p := tea.NewProgram(ui.New(), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		log.Fatal(err)
 	}
-	logger.InfoLog.Println("The program is running")
 }
